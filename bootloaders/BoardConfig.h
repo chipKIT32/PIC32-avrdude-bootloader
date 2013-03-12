74934f5e19e3cbc5b75b1fb5ead70e0f44ee912c
Brian Schmalz <brian@schmalzhaus.com>
2013-03-11 21:02:42 -0500

    Starting new bootloader variant for Fubarino Mini v1.5

--- bootloaders/BoardConfig.h
+++ bootloaders/BoardConfig.h
@@ -73,6 +73,7 @@
 #define     prodFubarinoSD      0x0001
 #define     prodFubarinoMini    0x0002
 #define     prodFubarinoSD_512K 0x0003
+#define     prodFubarinoMini50MHz 0x0004
 
 // *****************************************************************************
 // *****************************************************************************
@@ -1366,6 +1367,71 @@
     #define VEND                        vendFubarino
     #define PROD                        prodFubarinoMini
     #define F_CPU                       40000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 (0x20000-0x1000)		    // Leave room one flash block (for bootloader!)
+    #define FLASH_PAGE_SIZE             1024						// In bytes
+
+//************************************************************************
+#elif defined(_BOARD_FUBARINO_MINI_USB_50MHZ_)
+ 
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //* Oscillator Settings
+    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    #pragma config POSCMOD  = XT                                    // Primary oscillator mode
+    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
+    #pragma config FPLLMUL  = MUL_25                                // PLL multiplier
+    #pragma config FPLLODIV = DIV_2                                 // PLL output divider
+    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
+    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+    
+    //* Clock control settings
+    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+    
+    //* USB Settings
+    #pragma config UPLLEN   = ON                                    // USB PLL enable
+    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
+    
+    //* Other Peripheral Device settings
+    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
+    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+    #pragma config WINDIS   = OFF
+    #pragma config JTAGEN   = OFF                          			// JTAG port disabled
+    
+    //* Code Protection settings
+    #pragma config CP       = OFF                                   // Code protection
+    #pragma config BWP      = OFF                                   // Boot flash write protect
+    #pragma config PWP      = OFF                                   // Program flash write protect
+    
+    //*    Debug settings
+    #pragma config ICESEL   = ICS_PGx1                      		// ICE/ICD Comm Channel Select
+    //#pragma config DEBUG    = ON                          		// DO NOT SET THIS CONFIG BIT, it will break debugging
+
+    #pragma config PMDL1WAY = OFF                           		// Allow multiple reconfigurations
+    #pragma config IOL1WAY  = OFF                           		// Allow multiple reconfigurations
+#endif
+    
+    #define CAPABILITIES    (blCapSplitFlashBootloader | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // Boot LED
+    #define EnableBootLED()             (TRISACLR = (1 << 10))
+    #define DisableBootLED()            (TRISASET = (1 << 10))
+    #define BootLED_Toggle()            (LATAINV = (1 << 10))
+    #define BootLED_On()                (LATASET = (1 << 10))
+    #define BootLED_Off()               (LATACLR = (1 << 10))
+
+    // Other capabilities
+    #define fLoadFromAVRDudeViaProgramButton        (PORTAbits.RA8 == 0) 
+    #define fLoadFromAVRDudeViaVirtualProgramButton (LATAbits.LATA8 == 1) 
+    #define ClearVirtualProgramButton()             (LATACLR = (1 << 8))
+  
+    #define _CPU_NAME_                  "32MX250F128D"
+    #define VEND                        vendFubarino
+    #define PROD                        prodFubarinoMini50MHz
+    #define F_CPU                       50000000UL
     #define F_PBUS                      F_CPU
 
     #define FLASH_BYTES                 (0x20000-0x1000)		    // Leave room one flash block (for bootloader!)
