cdf7e8a0d17808d0c31f68c0c5460f8fc63aa355
Matt Jenkins <matt@majenko.co.uk>
2013-11-12 00:34:46 +0000

    Added Pontech Quick240 configuration

--- bootloaders/BoardConfig.h
+++ bootloaders/BoardConfig.h
@@ -51,8 +51,8 @@
 #define     vendSchmalzHausLLC  0x0004
 #define     vendOlimex          0x0005
 #define		vendElement14		0x0006
+#define     vendPontech         0x0007
 #define     vendExperimental    0x8000
-
 // *****************************************************************************
 // *****************************************************************************
 //                    Digilent Product IDs
@@ -105,6 +105,13 @@
 // *****************************************************************************
 // *****************************************************************************
 #define     prodChipkitPi		0x0001
+
+// *****************************************************************************
+// *****************************************************************************
+//                    Pontech Product IDs
+// *****************************************************************************
+// *****************************************************************************
+#define     prodQuick240                0x0001
 
 // Bootloader Capability bits
 // The first 4 bits define what type of STK500 interface is used
@@ -1324,6 +1331,68 @@
     #define _CPU_NAME_                  "32MX795F512H"
     #define VEND                        vendFubarino
     #define PROD                        prodFubarinoSD_512K
+    #define F_CPU                       80000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 0x80000                     // 512K
+    #define FLASH_PAGE_SIZE             4096
+    #define LoadFlashWaitStates()       (CHECON = 2)            // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
+
+//************************************************************************
+#elif defined(_BOARD_QUICK240_)
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //* Oscillator Settings
+    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    #pragma config POSCMOD  = HS                                    // Primary oscillator mode
+    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
+    #pragma config FPLLMUL  = MUL_20                                // PLL multiplier
+    #pragma config FPLLODIV = DIV_1                                 // PLL output divider
+    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
+    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+
+    //* Clock control settings
+    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+
+    //* USB Settings
+    #pragma config UPLLEN   = ON                                    // USB PLL enable
+    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
+    #pragma config FUSBIDIO = OFF									// USB USID pin controlled by port function
+    #pragma config FVBUSONIO = OFF									// USB VBUSON pin controlled by port function
+
+    //* Other Peripheral Device settings
+    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
+    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+
+    //* Code Protection settings
+    #pragma config CP       = OFF                                   // Code protection
+    #pragma config BWP      = OFF                                   // Boot flash write protect
+    #pragma config PWP      = OFF                                   // Program flash write protect
+
+    //* Debug settings
+    #pragma config ICESEL   = ICS_PGx2                              // ICE pin selection
+#endif
+
+    #define CAPABILITIES    (blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // Boot LED
+    #define EnableBootLED()             (TRISECLR = (1 << 0))
+    #define DisableBootLED()            (TRISESET = (1 << 0))
+    #define BootLED_Toggle()            (LATEINV = (1 << 0))
+    #define BootLED_On()                (LATESET = (1 << 0))
+    #define BootLED_Off()               (LATECLR = (1 << 0))
+
+    // Other capabilities
+    #define fLoadFromAVRDudeViaProgramButton        (PORTGbits.RG15 == 0)
+    #define fLoadFromAVRDudeViaVirtualProgramButton (LATGbits.LATG15 == 1)
+    #define ClearVirtualProgramButton()             (LATGCLR = (1 << 15))
+
+    #define _CPU_NAME_                  "32MX795F512H"
+    #define VEND                        vendPontech
+    #define PROD                        prodQuick240
     #define F_CPU                       80000000UL
     #define F_PBUS                      F_CPU
 
