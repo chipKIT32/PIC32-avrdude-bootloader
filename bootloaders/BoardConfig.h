69a1ce95d8ff8aa6d70db790a1374a6570777ac2
Matt Jenkins <matt@majenko.co.uk>
2013-11-15 12:01:11 +0000

    Fixed Quick240 ethernet pin configuration

--- bootloaders/BoardConfig.h
+++ bootloaders/BoardConfig.h
@@ -1838,37 +1838,44 @@
 
 #if defined(PUT_CONFIG_BITS_HERE)
 
-    //* Oscillator Settings
-    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
-    #pragma config POSCMOD  = HS                                    // Primary oscillator mode
-    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
-    #pragma config FPLLMUL  = MUL_20                                // PLL multiplier
-    #pragma config FPLLODIV = DIV_1                                 // PLL output divider
-    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
-    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+    //*    Oscillator Settings
+    #pragma config FNOSC        = PRIPLL                            // Oscillator selection
+    #pragma config POSCMOD      = XT                                // Primary oscillator mode
+    #pragma config FPLLIDIV     = DIV_2                             // PLL input divider
+    #pragma config FPLLMUL      = MUL_20                            // PLL multiplier
+    #pragma config FPLLODIV     = DIV_1                             // PLL output divider
+    #pragma config FPBDIV       = DIV_1                             // Peripheral bus clock divider
+    #pragma config FSOSCEN      = OFF                               // Secondary oscillator enable
 
-    //* Clock control settings
-    #pragma config IESO     = OFF                                   // Internal/external clock switchover
-    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
-    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+    //*    Clock control settings
+    #pragma config IESO         = OFF                               // Internal/external clock switchover
+    #pragma config FCKSM        = CSDCMD                            // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC     = OFF                               // Clock output on OSCO pin enable
 
-    //* USB Settings
-    #pragma config UPLLEN   = ON                                    // USB PLL enable
-    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
-    #pragma config FUSBIDIO = OFF                                   // USB USID pin controlled by port function
-    #pragma config FVBUSONIO = OFF                                  // USB VBUSON pin controlled by port function
+    //*    Other Peripheral Device settings
+    #pragma config FWDTEN       = OFF                               // Watchdog timer enable
+    #pragma config WDTPS        = PS1024                            // Watchdog timer postscaler
 
-    //* Other Peripheral Device settings
-    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
-    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+    //*    Code Protection settings
+    #pragma config CP           = OFF                               // Code protection
+    #pragma config BWP          = OFF                               // Boot flash write protect
+    #pragma config PWP          = OFF                               // Program flash write protect
 
-    //* Code Protection settings
-    #pragma config CP       = OFF                                   // Code protection
-    #pragma config BWP      = OFF                                   // Boot flash write protect
-    #pragma config PWP      = OFF                                   // Program flash write protect
+    //*    Debug settings
+    #pragma config ICESEL       = ICS_PGx2                          // ICE pin selection
 
-    //* Debug settings
-    #pragma config ICESEL   = ICS_PGx2                              // ICE pin selection
+    //*    Other Peripheral Device settings
+    #pragma config FSRSSEL      = PRIORITY_7                        // SRS interrupt priority
+    #pragma config FCANIO       = OFF                               // Standard/alternate CAN pin select (OFF=Alt)
+    #pragma config FETHIO       = ON                                // Standard/alternate ETH pin select (OFF=Alt)
+    #pragma config FMIIEN       = OFF                               // MII/RMII select (OFF=RMII)
+
+    //*    USB Settings
+    #pragma config UPLLEN       = ON                                // USB PLL enable
+    #pragma config UPLLIDIV     = DIV_2                             // USB PLL input divider
+    #pragma config FVBUSONIO    = OFF                               // VBUS pin control
+    #pragma config FUSBIDIO     = OFF                               // USBID pin control
+
 #endif
 
     #define CAPABILITIES    (blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
