43f93e4ea3dc6e4a8c24b7a805c8cfabcdd5ce6e
Brian Schmalz <brian@schmalzhaus.com>
2014-05-09 20:42:42 -0500

    Updated rest of bootloader source code to BSD license. I got permission from Rich Testardi (the code's original author) for this change. This brings the entire bootloader repo up to the same license.

--- bootloaders/main.c
+++ bootloaders/main.c
@@ -1,13 +1,41 @@
-// *** main.c ******************************************************************
-//
-// this is the main program that is launched by crt0.S; it just
-// initializes all of the modules of the os and then runs the main
-// application program loop.
-//
-// This file originated from the cpustick.com skeleton project from
-// http://www.cpustick.com/downloads.htm and was originally written
-// by Rich Testardi; please preserve this reference and share bug
-// fixes with rich@testardi.com.
+/**** main.c ******************************************************************
+ *
+ * this is the main program that is launched by crt0.S; it just
+ * initializes all of the modules of the os and then runs the main
+ * application program loop.
+ *
+ * This file originated from the cpustick.com skeleton project from
+ * http://www.cpustick.com/downloads.htm and was originally written
+ * by Rich Testardi; please preserve this reference and share bug
+ * fixes with rich@testardi.com.
+ * Copyright 2014 Rich Testardi
+ *
+ * This program is free software; distributed under the terms of
+ * BSD 3-clause license ("Revised BSD License", "New BSD License", or "Modified BSD License")
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1.    Redistributions of source code must retain the above copyright notice, this
+ *        list of conditions and the following disclaimer.
+ * 2.    Redistributions in binary form must reproduce the above copyright notice,
+ *        this list of conditions and the following disclaimer in the documentation
+ *        and/or other materials provided with the distribution.
+ * 3.    Neither the name(s) of the above-listed copyright holder(s) nor the names
+ *        of its contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #define PUT_CONFIG_BITS_HERE
 #include "main.h"
