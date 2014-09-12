0aac6aa444614262cd80091e47482fe355e537ee
Jacob Christ <jacob@pontech.com>
2014-09-11 21:14:58 -0700

    Fixes for PONTECH-Quick240 in BoardConfig.h (Missing some Ethernet settings).

--- bootloaders/BoardConfig.h
+++ bootloaders/BoardConfig.h
@@ -2662,19 +2662,19 @@
 
 #if defined(PUT_CONFIG_BITS_HERE)
 
-    //* Oscillator Settings
-    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    //*    Oscillator Settings
+    #pragma config FNOSC        = PRIPLL                            // Oscillator selection
     #pragma config POSCMOD  = HS                                    // Primary oscillator mode
-    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
-    #pragma config FPLLMUL  = MUL_20                                // PLL multiplier
-    #pragma config FPLLODIV = DIV_1                                 // PLL output divider
-    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
-    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+    #pragma config FPLLIDIV     = DIV_2                             // PLL input divider
+    #pragma config FPLLMUL      = MUL_20                            // PLL multiplier
+    #pragma config FPLLODIV     = DIV_1                             // PLL output divider
+    #pragma config FPBDIV       = DIV_1                             // Peripheral bus clock divider
+    #pragma config FSOSCEN      = OFF                               // Secondary oscillator enable
 
-    //* Clock control settings
-    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    //*    Clock control settings
+    #pragma config IESO         = OFF                               // Internal/external clock switchover
     #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
-    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+    #pragma config OSCIOFNC     = OFF                               // Clock output on OSCO pin enable
 
     //* USB Settings
     #pragma config UPLLEN   = ON                                    // USB PLL enable
@@ -2682,17 +2682,24 @@
     #pragma config FUSBIDIO = OFF                                   // USB USID pin controlled by port function
     #pragma config FVBUSONIO = OFF                                  // USB VBUSON pin controlled by port function
 
-    //* Other Peripheral Device settings
-    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
-    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+    //*    Other Peripheral Device settings
+    #pragma config FWDTEN       = OFF                               // Watchdog timer enable
+    #pragma config WDTPS        = PS1024                            // Watchdog timer postscaler
 
-    //* Code Protection settings
-    #pragma config CP       = OFF                                   // Code protection
-    #pragma config BWP      = OFF                                   // Boot flash write protect
-    #pragma config PWP      = OFF                                   // Program flash write protect
+    //*    Code Protection settings
+    #pragma config CP           = OFF                               // Code protection
+    #pragma config BWP          = OFF                               // Boot flash write protect
+    #pragma config PWP          = OFF                               // Program flash write protect
 
-    //* Debug settings
-    #pragma config ICESEL   = ICS_PGx2                              // ICE pin selection
+    //*    Debug settings
+    #pragma config ICESEL       = ICS_PGx2                          // ICE pin selection
+
+    //*    Other Peripheral Device settings
+    #pragma config FSRSSEL      = PRIORITY_7                        // SRS interrupt priority
+    #pragma config FCANIO       = OFF                               // Standard/alternate CAN pin select (OFF=Alt)
+    #pragma config FETHIO       = ON                                // Standard/alternate ETH pin select (OFF=Alt)
+    #pragma config FMIIEN       = OFF                               // MII/RMII select (OFF=RMII)
+
 #endif
 
     #define CAPABILITIES    (blCapBootLED | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
@@ -2705,13 +2712,22 @@
     #define BLedLat     E
     #define BLedBit     0
 
-     // Virtual program button
+    // Download LED
+    #define DLedLat     A
+    #define DLedBit     7
+
+    // Virtual program button
     #define VPBntLat    G
     #define VPBntBit    15
 
     // Program button
     #define PBntPort    G
     #define PBntBit     15
+
+    // Other capabilities
+    //#define LISTEN_BEFORE_LOAD          2000                // no less than 2 seconds
+    //#define BOOTLOADER_UART             1                   // avrdude program UART
+    //#define BAUDRATE                    115200              // avrdude baudrate
 
     #define _CPU_NAME_                  "32MX795F512L"
     #define VEND                        vendPontech
@@ -2721,7 +2737,7 @@
 
     #define FLASH_BYTES                 0x80000                     // 512K
     #define FLASH_PAGE_SIZE             4096
-    #define LoadFlashWaitStates()       (CHECON = 2)            // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
+    #define LoadFlashWaitStates()       (CHECON = 2)                // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
 
 //************************************************************************
 #elif defined(_BOARD_CHIPKIT_PI_USB_)
