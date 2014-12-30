60a71c72765256d0314e5e9b3196c9d9cd165b93
Matt Jenkins <matt@majenko.co.uk>
2014-12-30 20:50:36 +0000

    Added two main Majenko boards

--- bootloaders/BoardConfig.h
+++ bootloaders/BoardConfig.h
@@ -83,6 +83,7 @@
 #define	    vendElement14       0x0006
 #define     vendPontech         0x0007
 #define     vend4DSystems       0x0008
+#define     vendMajenko         0x0009
 #define     vendExperimental    0x8000
 
 // *****************************************************************************
@@ -172,6 +173,14 @@
 // *****************************************************************************
 // *****************************************************************************
 #define     prodPICadillo35t            0x0001
+
+// *****************************************************************************
+// *****************************************************************************
+//                    Majenko Technologies Product IDs
+// *****************************************************************************
+// *****************************************************************************
+#define     prodSDXL                    0x0001
+#define     prodSDAU                    0x0002
 
 // Bootloader Capability bits
 // The first 4 bits define what type of STK500 interface is used
@@ -2954,6 +2963,142 @@
     #define FLASH_BYTES                 0x80000                     // 512K
     #define FLASH_PAGE_SIZE             4096
     #define LoadFlashWaitStates()       (CHECON = 2)                // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
+
+//************************************************************************
+#elif defined(_BOARD_MAJENKO_SDXL_)
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //* Oscillator Settings
+    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    #pragma config POSCMOD  = EC                                    // Primary oscillator mode
+    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
+    #pragma config FPLLMUL  = MUL_20                                // PLL multiplier
+    #pragma config FPLLODIV = DIV_1                                 // PLL output divider
+    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
+    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+
+    //* Clock control settings
+    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+
+    //* USB Settings
+    #pragma config UPLLEN   = ON                                    // USB PLL enable
+    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
+    #pragma config FUSBIDIO = OFF                                   // USB USID pin controlled by port function
+    #pragma config FVBUSONIO = OFF                                  // USB VBUSON pin controlled by port function
+
+    //* Other Peripheral Device settings
+    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
+    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+
+    //* Code Protection settings
+    #pragma config CP       = OFF                                   // Code protection
+    #pragma config BWP      = OFF                                   // Boot flash write protect
+    #pragma config PWP      = OFF                                   // Program flash write protect
+
+    //* Debug settings
+    #pragma config ICESEL   = ICS_PGx2                              // ICE pin selection
+#endif
+
+    #define CAPABILITIES    (blCapBootLED | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // BTN / LED sense
+    #define LedOn       High
+    #define BntOn       Low
+
+    // Boot LED
+    #define BLedLat     E
+    #define BLedBit     7
+
+    // Virtual program button
+    #define VPBntLat    B
+    #define VPBntBit    5
+
+    // Program button
+    #define PBntPort    B
+    #define PBntBit     5
+
+    #define _CPU_NAME_                  "32MX795F512H"
+    #define VEND                        vendMajenko
+    #define PROD                        prodSDXL
+    #define F_CPU                       80000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 0x80000                     // 512K
+    #define FLASH_PAGE_SIZE             4096
+    #define LoadFlashWaitStates()       (CHECON = 2)            // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
+
+//************************************************************************
+#elif defined(_BOARD_MAJENKO_SDAU_)
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //* Oscillator Settings
+    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    #pragma config POSCMOD  = EC                                    // Primary oscillator mode
+    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
+    #pragma config FPLLMUL  = MUL_24                                // PLL multiplier
+    #pragma config FPLLODIV = DIV_1                                 // PLL output divider
+    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
+    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+
+    //* Clock control settings
+    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+
+    //* USB Settings
+    #pragma config UPLLEN   = ON                                    // USB PLL enable
+    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
+    #pragma config FUSBIDIO = OFF                                   // USB USID pin controlled by port function
+    #pragma config FVBUSONIO = OFF                                  // USB VBUSON pin controlled by port function
+
+    //* Other Peripheral Device settings
+    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
+    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+
+    // PPS
+    #pragma config PMDL1WAY = OFF                                   // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+    #pragma config IOL1WAY = OFF                                    // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+
+//* Code Protection settings
+    #pragma config CP       = OFF                                   // Code protection
+    #pragma config BWP      = OFF                                   // Boot flash write protect
+    #pragma config PWP      = OFF                                   // Program flash write protect
+
+    //* Debug settings
+    #pragma config ICESEL   = ICS_PGx2                              // ICE pin selection
+#endif
+
+    #define CAPABILITIES    (blCapBootLED | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // BTN / LED sense
+    #define LedOn       High
+    #define BntOn       Low
+
+    // Boot LED
+    #define BLedLat     E
+    #define BLedBit     7
+
+    // Virtual program button
+    #define VPBntLat    B
+    #define VPBntBit    5
+
+    // Program button
+    #define PBntPort    B
+    #define PBntBit     5
+
+    #define _CPU_NAME_                  "32MX470F512H"
+    #define VEND                        vendMajenko
+    #define PROD                        prodSDAU
+    #define F_CPU                       96000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 0x80000                     // 512K
+    #define FLASH_PAGE_SIZE             4096
+    #define LoadFlashWaitStates()       (CHECON = 2)            // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
 
 //************************************************************************
 
