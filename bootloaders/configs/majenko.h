c1926bfc41c87da094d0822321e6bd97ffde1cb6
Matt Jenkins <matt@majenko.co.uk>
2016-04-08 17:14:19 +0100

    Set lenny to 40MHz and added extra 48MHz config

--- bootloaders/configs/majenko.h
+++ bootloaders/configs/majenko.h
@@ -337,6 +337,84 @@
     #pragma config FNOSC    = PRIPLL                                // Oscillator selection
     #pragma config POSCMOD  = EC                                    // Primary oscillator mode
     #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
+    #pragma config FPLLMUL  = MUL_20                                // PLL multiplier
+    #pragma config FPLLODIV = DIV_2                                 // PLL output divider
+    #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
+    #pragma config FSOSCEN  = OFF                                   // Secondary oscillator enable
+
+    //* Clock control settings
+    #pragma config IESO     = OFF                                   // Internal/external clock switchover
+    #pragma config FCKSM    = CSECME                                // Clock switching (CSx)/Clock monitor (CMx)
+    #pragma config OSCIOFNC = OFF                                   // Clock output on OSCO pin enable
+
+    //* USB Settings
+    #pragma config UPLLEN   = ON                                    // USB PLL enable
+    #pragma config UPLLIDIV = DIV_2                                 // USB PLL input divider
+    #pragma config FVBUSONIO = OFF                                  // Make VBUSON a GPIO pin
+    #pragma config FUSBIDIO = OFF                                   // Controlled by port function
+
+    //* Other Peripheral Device settings
+    #pragma config FWDTEN   = OFF                                   // Watchdog timer enable
+    #pragma config WDTPS    = PS1024                                // Watchdog timer postscaler
+    #pragma config WINDIS   = OFF
+    #pragma config JTAGEN   = OFF                                   // JTAG port disabled
+
+    //* Code Protection settings
+    #pragma config CP       = OFF                                   // Code protection
+    #pragma config BWP      = OFF                                   // Boot flash write protect
+    #pragma config PWP      = OFF                                   // Program flash write protect
+
+    //*    Debug settings
+    #pragma config ICESEL   = ICS_PGx3                      		// ICE/ICD Comm Channel Select
+    //#pragma config DEBUG    = ON                          		// DO NOT SET THIS CONFIG BIT, it will break debugging
+
+    #pragma config PMDL1WAY = OFF                           		// Allow multiple reconfigurations
+    #pragma config IOL1WAY  = OFF                           		// Allow multiple reconfigurations
+#endif
+
+    #define CAPABILITIES    (blCapBootLED | blCapDownloadLED | blCapSplitFlashBootloader | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // BTN / LED sense
+    #define LedOn       High
+    #define BntOn       Low
+
+    // Boot LED
+    #define BLedLat     A
+    #define BLedBit     10
+
+    // Boot LED
+    #define DLedLat     A
+    #define DLedBit     8
+
+
+    // Virtual program button
+    #define VPBntLat    B
+    #define VPBntBit    4
+
+    // Program button
+    #define PBntPort    B
+    #define PBntBit     4
+    #define PBntPu      High
+
+    #define _CPU_NAME_                  "32MX270F256D"
+    #define VEND                        vendMajenko
+    #define PROD                        prodLenny
+    #define F_CPU                       40000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 (0x40000-0x1000)		    // Leave room 4 pages (for bootloader!)
+    #define FLASH_PAGE_SIZE             1024                                // In bytes
+#endif
+
+#if defined(_BOARD_MAJENKO_LENNY48_)
+#define _CONFIG_VALID_
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+    //* Oscillator Settings
+    #pragma config FNOSC    = PRIPLL                                // Oscillator selection
+    #pragma config POSCMOD  = EC                                    // Primary oscillator mode
+    #pragma config FPLLIDIV = DIV_2                                 // PLL input divider
     #pragma config FPLLMUL  = MUL_24                                // PLL multiplier
     #pragma config FPLLODIV = DIV_2                                 // PLL output divider
     #pragma config FPBDIV   = DIV_1                                 // Peripheral bus clock divider
@@ -365,7 +443,7 @@
     #pragma config PWP      = OFF                                   // Program flash write protect
 
     //*    Debug settings
-    #pragma config ICESEL   = ICS_PGx1                      		// ICE/ICD Comm Channel Select
+    #pragma config ICESEL   = ICS_PGx3                      		// ICE/ICD Comm Channel Select
     //#pragma config DEBUG    = ON                          		// DO NOT SET THIS CONFIG BIT, it will break debugging
 
     #pragma config PMDL1WAY = OFF                           		// Allow multiple reconfigurations
